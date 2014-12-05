754bc0931e4286dffb85070726b59677b0439acc
main() <main@ehvag.de>
2014-12-05 23:25:16 +0100

    Implemented HTML2Plaintext
    This converts HTML to Markdownish plaintext. Not perfect, but works well for simple cases.
    If your HTML is too complex for this, you probably shouldn't even try converting it to plaintext anyways.
    
    To see what it looks like in action, just take a look at the test case.
    
    @sohalt What do you think about this solution?

--- csharp/BridgeChat.ConversionFramework/Converters/HTML2Plaintext.cs
+++ csharp/BridgeChat.ConversionFramework/Converters/HTML2Plaintext.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 
 using BridgeChat.ConversionFramework.MessageType;
+
+using HtmlAgilityPack;
 
 namespace BridgeChat.ConversionFramework.Converters
 {
@@ -14,7 +20,169 @@ namespace BridgeChat.ConversionFramework.Converters
 
         public override Plaintext Convert(HTML input)
         {
-            throw new NotImplementedException();
+            var linkList = new List<string>();
+            var builder = new StringBuilder();
+            var document = new HtmlDocument();
+            document.LoadHtml(input.Content);
+            RunConversion(document.DocumentNode, builder, linkList);
+
+            // and postprocess: coerce multiple blank lines
+            var result = builder.ToString();
+            builder.Clear();
+            bool justHadWhitespace = false;
+            foreach (var part in result.Split('\n'))
+                if (!(justHadWhitespace & (justHadWhitespace = String.IsNullOrWhiteSpace(part))))
+                    builder.AppendLine(part);
+
+            if (linkList.Count > 0) {
+                builder.AppendLine();
+                for (int i = 0; i < linkList.Count; i++)
+                    builder.AppendFormat("[{0}]: {1}\n", i, linkList[i]);
+            }
+
+            return new Plaintext { Content = builder.ToString().Trim() };
+        }
+
+        private static void ConvertChildren(HtmlNode node, StringBuilder builder,
+            ICollection<string> linkList, string linePrefix = "")
+        {
+            foreach (var child in node.ChildNodes)
+                RunConversion(child, builder, linkList, linePrefix);
+        }
+
+        private static void HeadlineHelper(HtmlNode node, StringBuilder output,
+            ICollection<string> linkList, int level)
+        {
+            for (int i = 0; i < level; i++)
+                output.Append('#');
+            output.Append(' ');
+            ConvertChildren(node, output, linkList);
+            output.AppendLine();
+            output.AppendLine();
+        }
+
+        private static void RunConversion(HtmlNode node, StringBuilder output,
+            ICollection<string> linkList, string linePrefix = "")
+        {
+            switch (node.NodeType) {
+            case HtmlNodeType.Comment:
+                break; // ignore comments
+            case HtmlNodeType.Document:
+            case HtmlNodeType.Element:
+                switch (node.Name) {
+                case "h1":
+                    HeadlineHelper(node, output, linkList, 1);
+                    break;
+                case "h2":
+                    HeadlineHelper(node, output, linkList, 2);
+                    break;
+                case "h3":
+                    HeadlineHelper(node, output, linkList, 3);
+                    break;
+                case "h4":
+                    HeadlineHelper(node, output, linkList, 4);
+                    break;
+                case "h5":
+                    HeadlineHelper(node, output, linkList, 5);
+                    break;
+                case "h6":
+                    HeadlineHelper(node, output, linkList, 6);
+                    break;
+                case "b":
+                case "strong":
+                    output.Append("__");
+                    ConvertChildren(node, output, linkList);
+                    output.Append("__");
+                    break;
+                case "i":
+                case "em":
+                    output.Append("*");
+                    ConvertChildren(node, output, linkList);
+                    output.Append("*");
+                    break;
+                case "ul":
+                    output.Append("\n\n");
+                    foreach (var li in node.ChildNodes) {
+                        if ((li.NodeType == HtmlNodeType.Element)
+                            && (li.Name == "li")) {
+                            output.Append("*   ");
+                            ConvertChildren(li, output, linkList);
+                            output.AppendLine();
+                        }
+                    }
+                    break;
+                case "ol":
+                    output.AppendLine();
+                    int olIndex = 1;
+                    foreach (var li in node.ChildNodes) {
+                        if ((li.NodeType == HtmlNodeType.Element)
+                            && (li.Name == "li")) {
+                            int oldLength = output.Length;
+                            output.Append(olIndex++).Append(".   ");
+                            output.Length = oldLength + 4;
+                            ConvertChildren(li, output, linkList);
+                            output.AppendLine();
+                        }
+                    }
+                    output.AppendLine();
+                    break;
+                case "pre":
+                    output.AppendLine();
+                    output.AppendLine();
+                    ConvertChildren(node, output, linkList, "    ");
+                    output.AppendLine();
+                    break;
+                case "p":
+                    ConvertChildren(node, output, linkList);
+                    output.AppendLine();
+                break;
+                case "hr":
+                    output.AppendLine();
+                    output.AppendLine("----------");
+                    output.AppendLine();
+                    break;
+                case "a":
+                    var href = node.Attributes.AttributesWithName("href").SingleOrDefault();
+                    if (href != null) {
+                        output.Append('[');
+                        ConvertChildren(node, output, linkList);
+                        output.Append("][").Append(linkList.Count).Append(']');
+                        linkList.Add(href.Value);
+                    } else // don't care
+                        ConvertChildren(node, output, linkList);
+                    break;
+                case "img":
+                    var imgSrc = node.Attributes.AttributesWithName("src").Single();
+                    var alt = node.Attributes.AttributesWithName("alt").SingleOrDefault();
+                    var altText = (alt != null) ? alt.Value : "Image";
+                    output.AppendFormat("![{0}][{1}]", altText, linkList.Count);
+                    linkList.Add(imgSrc.Value);
+                    break;
+                case "br":
+                    output.AppendLine();
+                    break;
+                default:
+                    // strip unknown tags and process what's inside of them
+                    ConvertChildren(node, output, linkList);
+                    break;
+                    }
+                break;
+            case HtmlNodeType.Text:
+                // just plain text
+                var inner = ((HtmlTextNode)node).Text;
+                if (!HtmlNode.IsOverlappedClosingElement(inner)
+                    && (inner.Trim().Length > 0)) {
+                    var lines = HtmlEntity.DeEntitize(inner).Split('\n');
+                    if (output[output.Length - 1] == '\n')
+                        output.Append(linePrefix);
+                    foreach (var line in lines)
+                        output.AppendLine(line).Append(linePrefix);
+                    output.Length -= linePrefix.Length + 1; // remove trailing newline and prefix
+                }
+                break;
+            default:
+                throw new NotImplementedException();
+            }
         }
     }
 }
--- csharp/UnitTests/TestConversionManager.cs
+++ csharp/UnitTests/TestConversionManager.cs
@@ -24,6 +24,19 @@ namespace UnitTests
             Assert.AreEqual(1, result.Count(x => x.GetType() == typeof(ImageLink))); // they do the conversion
             Assert.AreEqual(1, result.Count(x => x.GetType() == typeof(Plaintext))); // but also pass the optional
         }
+
+        [Test]
+        public void Html2Text()
+        {
+            var result = ConversionManager.Instance.RunConversion(new object[] {
+                new HTML { Content = "<h1>Head<em>l</em>ine</h1><a href=\"http://ehvag.de/\">anchoring</a><br>Here you go, have an image:<img src=\"https://www.google.de/images/srpr/logo11w.png\"><pre>some code\neven <strong>more</strong> code\n even with whitespace \n</pre><p><ul><li>liul1</li><li>l<strong>iu</strong>l2</li></ul><ol><li>lio<em>l1</em></li><li>liol2</li></ol>si<strong>mp<em>l</em>et</strong>ext</p>" }
+            }, new Type[] {
+                typeof(Plaintext)
+            }, new Type[] { });
+
+            var contentResult = ((Plaintext)result.Single()).Content;
+            Assert.AreEqual("# Head*l*ine\n\n[anchoring][0]\nHere you go, have an image:![Image][1]\n\n    some code\n    even __more__ code\n     even with whitespace \n    \n*   liul1\n*   l__iu__l2\n\n1.  lio*l1*\n2.  liol2\n\nsi__mp*l*et__ext\n\n\n[0]: http://ehvag.de/\n[1]: https://www.google.de/images/srpr/logo11w.png", contentResult);
+        }
     }
 }
 
